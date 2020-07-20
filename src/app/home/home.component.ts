11c131863c20c18e5fc106b81016df4f6c8a6454
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-20 08:16:45 -0300

    ajuste7

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  title = 'home';	
+  public rota: Router;
+  public resumo: any;
+  readonly apiURL : string;
+  @Input()
+  public session: any;
 
-  constructor() { }
+  constructor(private http : HttpClient, private r: Router){
+    this.apiURL = 'https://keen-curie-febfbf.netlify.app';
+    this.rota = r;
+  }
 
   ngOnInit(): void {
+    this.session = JSON.parse(window.localStorage.getItem('currentUser'));
+    console.log(this.session)
+
+    this.http.get(`${this.apiURL}/home`)
+      .subscribe(result => {
+        this.resumo = result;
+        console.log(this.resumo)
+      });
   }
 
 }
